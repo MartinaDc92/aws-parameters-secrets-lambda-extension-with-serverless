912da9cc69a112cb020070dabea2936a250b5698
martina-ep <60542169+MartinaDc92@users.noreply.github.com>
2022-10-23 20:05:13 +0200

    🎨 imrpoved naming

--- src/displaySecret/handler.ts
+++ src/displaySecret/handler.ts
@@ -1,7 +1,7 @@
 import got from 'got';
 import * as AWS from 'aws-sdk'; 
 
-export const displaySecrets = async () => {
+export const displaySecret = async () => {
     const secretsExtensionEndpoint = `http://localhost:2773/secretsmanager/get?secretId={your-secret-id}`;
     const headers = {
         "X-Aws-Parameters-Secrets-Token": process.env.AWS_SESSION_TOKEN
